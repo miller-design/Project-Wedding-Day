b2085ee917ccfddb23fb051b9005c036c55c67cb
JackMiller <jack.miller@live.co.uk>
2025-03-08 10:34:39 +0000

    fix: moved lock screen into pages template to render metadata even when locked

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -3,10 +3,14 @@ import type { Metadata } from "next";
 import { CaptchaProvider } from "@/components/CaptchaProvider";
 import { TooBlockLoop } from "@/components/TooBlockLoop";
 
-import { queryBySlug } from "@/lib/Payload/queries";
+import { queryBySlug, queryGlobal } from "@/lib/Payload/queries";
 
 import { Args } from "./type";
 import { isPageType } from "@/lib/utils";
+import { hasCookie } from "cookies-next";
+import { cookies } from "next/headers";
+import { Media } from "@/payload-types";
+import { LoginForm } from "@/components/UI/Forms";
 
 /**
  * This function generates metadata for a page based on its slug. It queries the Payload CMS for a page with the given slug,
@@ -33,29 +37,54 @@ export async function generateMetadata({ params: paramsPromise }: Args): Promise
  * queries the Payload CMS for the page content, and returns a JSX component representing the page.
  */
 const Page = async ({ params: paramsPromise }: Args) => {
+	const allowAccess = await hasCookie("jp_allow_access", { cookies });
 	const { slug } = await paramsPromise;
 	const effectiveSlug = slug ?? "home";
 
-	const page = await queryBySlug({ collection: "pages", slug: effectiveSlug });
+	if(allowAccess) {
+		const page = await queryBySlug({ collection: "pages", slug: effectiveSlug });
 
-	if (!isPageType(page)) {
-		return <></>;
+		if (!isPageType(page)) {
+			return <></>;
+		}
+
+		const { hero, content } = page;
+		const blocks = [...(hero?.blocks || []), ...(content?.blocks || [])];
+
+		return (
+			<article className="[ min-h-screen ][ mb-20-30 ]">
+				{blocks.length > 0 && (
+					<div className="[ too-grid too-row-gap ]">
+						<CaptchaProvider>
+							<TooBlockLoop blocks={blocks} />
+						</CaptchaProvider>
+					</div>
+				)}
+			</article>
+		);
+	} else {
+		const globals = await queryGlobal();
+		const { password, lockscreen_bg } = globals;
+		const weddingPW = password;
+		const weddingBg = lockscreen_bg as Media;
+		const weddingBgProps = {
+			image: {
+				src: weddingBg?.url as string,
+				alt: weddingBg?.alt as string
+			},
+			width: weddingBg?.width,
+			height: weddingBg?.height,
+			sizes: [90],
+			priority: true,
+			intrinsic: false
+		};
+
+		return (
+			<CaptchaProvider>
+				<LoginForm password={weddingPW} bg={weddingBgProps} />
+			</CaptchaProvider>
+		)
 	}
-
-	const { hero, content } = page;
-	const blocks = [...(hero?.blocks || []), ...(content?.blocks || [])];
-
-	return (
-		<article className="[ min-h-screen ][ mb-20-30 ]">
-			{blocks.length > 0 && (
-				<div className="[ too-grid too-row-gap ]">
-					<CaptchaProvider>
-						<TooBlockLoop blocks={blocks} />
-					</CaptchaProvider>
-				</div>
-			)}
-		</article>
-	);
 };
 
 export default Page;
--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -9,16 +9,12 @@ import { cookies } from "next/headers";
 
 import { hasCookie } from "cookies-next/server";
 
-import { CaptchaProvider } from "@/components/CaptchaProvider";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { SmoothScroll } from "@/components/SmoothScroll";
-import { LoginForm } from "@/components/UI/Forms";
 
 import { bodoniModa, poppins } from "@/lib/fonts";
 import { queryGlobal } from "@/lib/Payload/queries";
-
-import { Media } from "@/payload-types";
 
 export async function generateMetadata(): Promise<Metadata> {
 	const globals = await queryGlobal();
@@ -58,43 +54,17 @@ export default async function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	const globals = await queryGlobal();
 	const allowAccess = await hasCookie("jp_allow_access", { cookies });
-	const { password, lockscreen_bg } = globals;
-	const weddingPW = password;
-	const weddingBg = lockscreen_bg as Media;
-	const weddingBgProps = {
-		image: {
-			src: weddingBg?.url as string,
-			alt: weddingBg?.alt as string
-		},
-		width: weddingBg?.width,
-		height: weddingBg?.height,
-		sizes: [90],
-		priority: true,
-		intrinsic: false
-	};
 
 	return (
 		<html lang="en">
 			<Provider>
 				<body className={clsx(bodoniModa.variable, poppins.variable, "antialiased")}>
-					{allowAccess ? (
-						<>
-							<Header />
-							<SmoothScroll>
-								<main className="[ too-col ][ min-h-screen ]">{children}</main>
-								<Footer />
-							</SmoothScroll>
-						</>
-					) : (
-						<>
-							<CaptchaProvider>
-								<Header isLocked={true} />
-								<LoginForm password={weddingPW} bg={weddingBgProps} />
-							</CaptchaProvider>
-						</>
-					)}
+					<Header isLocked={!allowAccess} />
+					<SmoothScroll>
+						<main className="[ too-col ][ min-h-screen ]">{children}</main>
+						<Footer />
+					</SmoothScroll>
 				</body>
 			</Provider>
 		</html>
