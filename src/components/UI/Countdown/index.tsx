0b650b251df37ddbe485cd606be1ea294e29ca68
JackMiller <jack.miller@live.co.uk>
2025-03-09 20:40:48 +0000

    feat: modal animation tweak

--- src/components/UI/Countdown/index.tsx
+++ src/components/UI/Countdown/index.tsx
@@ -56,11 +56,11 @@ const CountdownTimer = ({ date }: Props) => {
 	return (
 		<>
 		<aside
-			className={clsx(["[ fixed right-10 bottom-[60px] z-50 ][ flex items-center justify-center gap-10-20 ] [ rounded-lg bg-white text-black ][ p-20-30 shadow-lg ][ transition-all duration-300 ]", isActive ? "opacity-100 translate-y-[0] " : "opacity-[0] translate-y-[10px]" ])}
+			className={clsx(["[ fixed right-10 bottom-[60px] z-50 ][ flex items-center justify-center gap-10-20 ] [ rounded-lg bg-white text-black ][ p-20-30 shadow-lg ][ transition-all duration-300 origin-bottom-right ]", isActive ? "opacity-100 scale-[1]" : "opacity-[0] scale-[0.95]" ])}
 		>
 			<p className="[ text-42-50 too-mono leading-[1] slashed-zero tabular-nums ]">{timeLeft}</p>
 		</aside>
-		<button className="[ rounded-md ][ fixed right-10 bottom-10 z-60 w-[40px] h-[40px] ][ bg-white text-black ][ shadow-md ]" onClick={toggleState}>
+		<button className="[ rounded-md ][ fixed right-10 bottom-[12px] z-60 w-[40px] h-[40px] ][ bg-white text-black ][ shadow-md ]" onClick={toggleState}>
 			<span className="sr-only">Close</span>
 			<Cross className={clsx(["[ too-abs-xy w-[30px] h-[30px] ][ hover:opacity-50 transition-opacity duration-300 ]", !isActive ? "hidden" : ""])} />
 			<WatchIcon className={clsx(["too-abs-xy w-[25px] h-[25px] ][ hover:opacity-50 transition-opacity duration-300 ]", isActive ? "hidden" : ""])}/>
